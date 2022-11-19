382bd069acd35500ed162669a543a7595cb00c28
MahadevanRamasamy <117591237+MahadevanRamasamy@users.noreply.github.com>
2022-11-20 00:56:26 +0530

    Add files via upload

---
+++ Week3Day1 Assignments/College.java
@@ -0,0 +1,21 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("College name is PSG");
+	}
+	public void collegeCode()
+	{
+		System.out.println("College code is 054");
+	}
+	public void collegeRank()
+	{
+		System.out.println("College rank is 5");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
